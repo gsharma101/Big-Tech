87d1a8236dfb4abe507e36e98478275519ce7a41
Gaurav Sharma <56496704+gsharma101@users.noreply.github.com>
2022-04-29 21:08:50 +0530

    updating L3

--- L3.cpp
+++ L3.cpp
@@ -1,8 +1,30 @@
 #include <bits\stdc++.h>
-
 using namespace std;
 
+vector <int> twoSum(vector<int>& nums, int target){
+        int a,b;
+        a = b = 0;
+        for(int i = 0; i<nums.size()-1; ++i)
+        {
+            for(int j = i + 1; j<nums.size(); ++j)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    a = i;
+                    b = j;
+                }   
+            }
+        }
+        return {a,b};
+}
+
 int main(){
-    
+    int target = 11;
+    vector <int> result;
+    vector <int> nums {1,3,7,9,2};
+    result = twoSum(nums, target);
+    for(int i = 0; i<=result.size()-1; i++){
+        cout<<result[i]<<" ";
+    }
     return 0;
 }
